70665ba27abb48d8103c4a18e549613f2cc0a1c9
restless <restless.e@gmail.com>
2016-03-28 04:31:10 -0700

    added dumb bot functionality. still going on wrong turn. but its late...will fix later. need smart bot.

--- GameOfNim/GameOfNim/GameOfNim.cpp
+++ GameOfNim/GameOfNim/GameOfNim.cpp
@@ -10,28 +10,56 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 //variables
 int totalMarbles, remainMarbles, takeMarbles, maxTakeMarbles, turn = 0;
-int pturn = 2;
-string player = "Player two";
+short pturn = 2;
+string player = "Player 2";
 
+
+//functions
 void gameFlow();
-int playerTurn(int pturn);
+int playerTurn();
 bool checkVictory(int remainMarbles);
-int TakeMarbs(int takeMarbles);
+
+//int TakeMarbs(int takeMarbles);
+void dumbBot()
+{
+	takeMarbles = rand() % maxTakeMarbles + 1;
+	cout << "CompPlayer has taken " << takeMarbles << " marbles." << endl;
+}
+void pickUpMarbles()
+{
+	cin >> takeMarbles;
+	if (!cin.good())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid input, please try again: " << endl;
+		pickUpMarbles();
+	}
+	else if (takeMarbles > remainMarbles)
+	{
+		cout << "Taking too many marbles, please take less." << endl;
+		pickUpMarbles();
+	}
+}
 
 int main() // game setup
 {
+	srand(time(NULL));
+
 	cout << "How many marbles do you want to play with?:" << endl;
 	cin >> totalMarbles;
 
 	remainMarbles = totalMarbles;
 	maxTakeMarbles = (remainMarbles / 2);// +0.5;
 
-	cout << "Excellent, we will play NIM with " << totalMarbles << " marbles!" << endl << endl;// << endl;
+	cout << "Excellent, we will play NIM with " << totalMarbles << " marbles!" << endl;
 	cout << "Player 1, we will begin with you. Please take at least 1 and up to " << maxTakeMarbles << " marbles." << endl;
 	cin >> takeMarbles;
 	//player has taken marbles - subtract and recount.
@@ -43,13 +71,12 @@ int main() // game setup
 
 	while (checkVictory(remainMarbles) == false)
 	{
-		//main(); // game setup
+		playerTurn();//function was below gameflow before, testing.
 		gameFlow();
-		playerTurn(pturn);
+		
 	}
 
 	cout << "Congratulations " << player << "! You have won!" << endl;
-	//sequence();
 
 	system("pause");
 	return 0;
@@ -61,31 +88,49 @@ void gameFlow()
 	maxTakeMarbles = (remainMarbles / 2) + 0.5;
 	cout << remainMarbles << " marbles remain." << endl;
 	cout << player << " it is your turn. You may take 1 - " << maxTakeMarbles << " marbles." << endl;
-	cin >> takeMarbles;
+	if (pturn == 2)
+	{
+		dumbBot();
+	}
+	else if (pturn == 1)
+	{
+		cin >> takeMarbles;
+	}
 	//TakeMarbs(cin) >> takeMarbles;
-	remainMarbles = remainMarbles - takeMarbles;
+	if ((remainMarbles - takeMarbles) > 1)
+		remainMarbles = remainMarbles - takeMarbles;
+	else if ((remainMarbles - takeMarbles) == 0)
+	{
+		cout << "you have lost" << endl;
+		//checkVictory = 1;
+	}
+	else if ((remainMarbles - takeMarbles) < 0)
+	{
+		cout << "Taking too many marbles, try taking less." << endl;
+		cin >> takeMarbles;
+	}
+
 }
 
-int playerTurn(int pturn)
+int playerTurn()
 {
+	//pturn = (pturn ? 1 : 2);
+
 	switch (pturn)
 	{	
 	case 1:
-		player = "Player two";
-		//	pturn = 2;
-		return 2;
+		player = "Player One";
+		pturn = 2;
 		break;
 	case 2:
-		player = "Player one";
-		//pturn = 1;
-		return 1;
+		player = "Player Two";
+		pturn = 1;
 		break;
 	default:
-		player = "Player one";
-		//pturn = 1;
-		return 1;
+		player = "Player unknown";
 		break;
 	}
+	return pturn;
 }
 
 bool checkVictory(int remainMarbles)
@@ -100,27 +145,28 @@ bool checkVictory(int remainMarbles)
 	}
 }
 
-int TakeMarbs(int takeMarbles)
-{
-	while (true)
-	{
-		cin >> takeMarbles;
-		if (takeMarbles > 1 && takeMarbles <= maxTakeMarbles)
-		{
-			break;
-		}
-		else if (takeMarbles < 1 && takeMarbles > maxTakeMarbles)
-		{
-			cin.clear();
-			cin.ignore();
-			cout << "Incorrect range. Try again: ";
-		}
-		else if (cin.fail())
-		{
-			cin.clear();
-			cin.ignore();
-			cout << "Incorrect entry. Try again: ";
-		}
-	}
-	return 0;
-}
+//
+//int TakeMarbs(int takeMarbles)
+//{
+//	while (true)
+//	{
+//		cin >> takeMarbles;
+//		if (takeMarbles > 1 && takeMarbles <= maxTakeMarbles)
+//		{
+//			break;
+//		}
+//		else if (takeMarbles < 1 && takeMarbles > maxTakeMarbles)
+//		{
+//			cin.clear();
+//			cin.ignore();
+//			cout << "Incorrect range. Try again: ";
+//		}
+//		else if (cin.fail())
+//		{
+//			cin.clear();
+//			cin.ignore();
+//			cout << "Incorrect entry. Try again: ";
+//		}
+//	}
+//	return 0;
+//}
